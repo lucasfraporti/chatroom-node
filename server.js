4114d7a280bd51008d89a6ae4f2fafdf3677fb4f
lucasfraporti <80408351+lucasfraporti@users.noreply.github.com>
2022-07-08 21:15:14 -0300

    update mural

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const formatMessage = require("./utils/messages");
-const {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers
-} = require("./utils/users");
+const {formatMessage, formatMessageRoomEnter, formatMessageRoomExit} = require("./utils/messages");
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -19,53 +14,42 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "Sistema 🤖";
 
-// Irá executar quando um cliente se conectar
+// Irá executar quando um cliente se conectar no servidor
 io.on("connection", (socket) => {
     socket.on("joinRoom", ({username, room}) => {
         // Entrando na sala e configurando o usuário no socket.io
         const user = userJoin(socket.id, username, room);
         socket.join(user.room);
 
-        // Broadcast quando um usuário se conectar
-        // Emite a mensagem para todos da sala específica menos para o usuário que está se conectando
+        // Unicast pois é uma mensagem para um usuário específico
+        socket.emit("message", formatMessage(botName, "Olá, utilize o nosso chat com moderação!", room));
+
+        // Broadcast quando um usuário se conectar, emitindo uma mensagem para todos usuários da sala específica menos o usuário que está conectando
         socket.broadcast
         .to(user.room)
         .emit(
             "message",
-            formatMessage(botName, `${user.username} entrou no chat.`),
-            // Mural da entrada de usuários no console
-            console.log(`${username} entrou na sala ${room}.`)
+            formatMessageRoomEnter(botName, `${user.username}`, room)
         );
 
-        // Enviando as informações do usuário e da sala
+        // Enviando pro servidor as informações da sala e do usuário
         io.to(user.room).emit("roomUsers", {
             room: user.room,
-            users: getRoomUsers(user.room),
+            users: getRoomUsers(user.room)
         });
-
-        // Unicast pois é uma mensagem para um usuário específico
-        socket.emit("message", formatMessage(botName, "Olá, utilize o nosso chat com moderação!"), 
-        // Mural exclusivo para o unicast da moderação no console
-        console.log(`${botName}: Olá, utilize o nosso chat com moderação!`));
     });
 
     // Procurando por uma mensagem
     socket.on("chatMessage", (msg) => {
         const user = getCurrentUser(socket.id);
-        io.to(user.room).emit("message", formatMessage(user.username, msg));
-
-        // Mural da envio de mensagem no console
-        console.log(`${user.username}: ${msg}`);
+        io.to(user.room).emit("message", formatMessage(user.username, msg, user.room));
 
         // Bot alerta o usuário específico para não mandar palavrões
         let bad_words = ["bobo", "boba"];
         for(i in bad_words){
             if(msg.toLowerCase().includes(bad_words[i])){
                 // Unicast
-                socket.emit("message", formatMessage(botName, "Pare de falar palavrões, seja mais educado! 🤫"));
-
-                // Mural exclusivo para o unicast de bad_words no console
-                console.log(`${botName}: Pare de falar palavrões, seja mais educado! 🤫`);
+                socket.emit("message", formatMessage(botName, "Pare de falar palavrões, seja mais educado! 🤫", user.room));
             }
         }
 
@@ -74,10 +58,7 @@ io.on("connection", (socket) => {
         for(i in good_words){
             if(msg.toLowerCase().includes(good_words[i])){
                 // Unicast
-                socket.emit("message", formatMessage(botName, "Obrigado, são seus olhos! 😍"));
-
-                // Mural exclusivo para o unicast de good_words no console
-                console.log(`${botName}: Obrigado, são seus olhos! 😍`);
+                socket.emit("message", formatMessage(botName, "Obrigado, são seus olhos! 😍", user.room));
             }
         }
     });
@@ -89,12 +70,10 @@ io.on("connection", (socket) => {
         if(user){
         io.to(user.room).emit(
             "message",
-            formatMessage(botName, `${user.username} saiu do chat.`),
-            // Mural da saída de usuários no console
-            console.log(`${user.username} saiu da sala ${user.room}.`)
+            formatMessageRoomExit(botName, `${user.username}`, user.room)
         );
 
-        // Enviando as informações do usuário
+        // Enviando as informações do usuário e da sala
         io.to(user.room).emit("roomUsers", {
             room: user.room,
             users: getRoomUsers(user.room)
--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,19 @@
 const moment = require('moment');
 
-function formatMessage(username, text){
+function formatMessage(username, text, room){
+    // Mural para o envio de mensagem em geral
+    console.log(`[${moment().format('H:m')}] - [${room}] - ${username}: ${text}`);
+    return {
+        username,
+        text,
+        time: moment().format('H:m')
+    };
+};
+
+function formatMessageRoomEnter(username, text, room){
+    // Mural para o envio de mensagem de entrada na sala
+    console.log(`[${moment().format('H:m')}] - [${room}] - ${username}: ${text} entrou na sala.`);
+    var text = `${text} entrou na sala.`;
     return {
         username,
         text,
@@ -8,4 +21,19 @@ function formatMessage(username, text){
     };
 }
 
-module.exports = formatMessage;
+function formatMessageRoomExit(username, text, room){
+    // Mural para o envio de mensagem de saída da sala
+    console.log(`[${moment().format('H:m')}] - [${room}] - ${username}: ${text} saiu da sala.`);
+    var text = `${text} saiu da sala.`;
+    return {
+        username,
+        text,
+        time: moment().format('H:m')
+    };
+}
+
+module.exports = {
+    formatMessage,
+    formatMessageRoomEnter,
+    formatMessageRoomExit
+};
